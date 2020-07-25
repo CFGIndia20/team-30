3bcfb71c9c1f862f33e436b3c73f4bcdee9cbfc7
adityajain17 <adtjn9@gmail.com>
2020-07-26 01:53:51 +0530

    Slot-Allocation-Done

--- backend/app.js
+++ backend/app.js
@@ -43,10 +43,7 @@ database:"cfg"
     
 app.get("/",function(req,res)
 {
-	conn.query("select * from users ",function(err,result)
-	{
-		res.send(result);
-	});
+	res.render("index.ejs");
 });
 app.get("/slots",isLoggedIn,function(req,res)
 {
@@ -54,20 +51,53 @@ app.get("/slots",isLoggedIn,function(req,res)
 	{
 		conn.query("select * from batch,slot where batch.slot_id=slot.id",function(err,result)
 		{
-			res.send(result);
+			// console.log(result);
+			res.render("slots.ejs",{result:result});
 		});
 	}
 	else
 	{
 		res.redirect("/");
 	}
-	});
+});
 app.post("/slots/:id",isLoggedIn,function(req,res)
 {
 	var bid=req.params.id;
-	conn.query("select count(*) from cfg.student_batch where batch_id="+bid+"group by batch_id",function(err,result)
+	console.log("The id is "+bid);
+	var sql="select count(*) from student_batch where batch_id="+bid+" group by batch_id";
+	// console.log(sql);
+	conn.query(sql,function(err,result)
 	{
-		console.log(result);
+		if(result.length==0)
+		{
+			conn.query("insert into student_batch (batch_id,student_username) values ("+bid+"\""+req.user.username+"\")",function(err,result)
+			{
+					req.flash("sucess","Slot is  Allocated ");
+					console.log("Slot Allocated ");
+					res.redirect("/");
+			});
+		}
+		else
+		{
+			var c=result[0]['count(*)']
+			if(c<15)
+			{
+				console.log("Entering Here");
+				var sql1="insert into student_batch (batch_id,student_username) values ("+bid+",\""+req.user.username+"\")";
+				conn.query(sql1,function(err,result2)
+				{
+					req.flash("sucess","Slot is  Allocated ");
+					console.log("Slot Allocated ");
+					res.redirect("/");
+
+				});
+			}
+			else
+			{
+				req.flash("error","Slot is  full");
+				res.redirect("/slots");
+			}
+		}
 	});
 });	
 app.get("/profile",isLoggedIn,function(req,res)
